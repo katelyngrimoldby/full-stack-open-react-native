efddfb10cafb9dcc6d229b137272b09d35438a15
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-06 13:54:10 -0700

    10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     black: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
     subheading: 20,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System'
+    })
   },
   fontWeights: {
     normal: '400',
