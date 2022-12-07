81f054f1f535bda40b7e65977165ba10252f921f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-07 12:07:24 -0700

    10.16

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,6 +1,10 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Pressable } from 'react-native';
+import { useQuery, useApolloClient } from '@apollo/client';
+import useAuthStorage from '../../hooks/useAuthStorage';
+import { ME } from '../../graphql/queries';
 import Constants from 'expo-constants';
 import theme from '../../theme';
+import Text from '../CustomText';
 import AppBarTab from './Tab';
 
 const styles = StyleSheet.create({
@@ -15,6 +19,14 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const client = useApolloClient();
+  const authStorage = useAuthStorage();
+  const { data } = useQuery(ME);
+
+  const handlePress = async () => {
+    await authStorage.removeAccessToken();
+    client.resetStore();
+  };
   return (
     <View style={styles.container}>
       <ScrollView
@@ -23,7 +35,20 @@ const AppBar = () => {
         contentContainerStyle={{ flexDirection: 'row', justifyContent: 'left' }}
       >
         <AppBarTab text='Repositories' route='/' />
-        <AppBarTab text='Sign In' route='/signin' />
+        {data && data.me ? (
+          <Pressable
+            style={{
+              marginLeft: 16,
+            }}
+            onPress={handlePress}
+          >
+            <Text color='light' fontSize='subheading'>
+              Sign Out
+            </Text>
+          </Pressable>
+        ) : (
+          <AppBarTab text='Sign In' route='/signin' />
+        )}
       </ScrollView>
     </View>
   );
